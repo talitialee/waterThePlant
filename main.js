4edf786c66e8cc5a98a43da2fb89223b16fac4c0
Alice Reuter <110020531+talitialee@users.noreply.github.com>
2022-12-23 13:38:34 +0100

    Add files via upload
    Added media queries
    BG color animation
    Added reload button
    Water button animation

--- main.js
+++ main.js
@@ -1,45 +1,79 @@
+// Generate a random number for the thirst level and display it //
 let thirstlevel = Math.floor((Math.random() * 21) + 5);
 
-// Generate a random number for the thirst level and display it //
+
 function thirstGenerator() {
   
   document.getElementById('thirst').innerHTML = thirstlevel;
-  if (thirstlevel < 50) {
-    document.getElementById('output').innerHTML = "I am thirsty !";
-  }
-   else {
-     document.getElementById('output').innerHTML = "I am good !";
-  }
+ 
   
-};
+}
 
-// Water the plant: button = thirstlevel + 10.//
+
+
 // Change the image if too watery + message + change color //
-function myFunction() {
-  thirstlevel += 10;
+function waterThePlant() {
+  thirstlevel += 15;
   document.getElementById('thirst').innerHTML = thirstlevel;
   if (thirstlevel > 70) {
-    document.getElementById('myPlant').src = 'resources/Plante4.png';
-    document.getElementById('output').innerHTML = "AaAaaAargh whyYyYyy";
-    document.getElementById('thirst').style.color = "var(--red)";
-    document.getElementById('output').style.color = "var(--red)";
-    document.getElementById('intro').style.display = "none";
-    document.getElementById('body').className = "bg2"; 
-    
+   fourthScene();     
   }
-
    
   else if (thirstlevel > 50) {
-   document.getElementById('output').innerHTML = "Moooore !"; document.getElementById('myPlant').src="resources/Plante3.png";
+   thirdScene();
   }
 
   else if (thirstlevel > 30) {
-    document.getElementById('output').innerHTML = "Moooore !"; document.getElementById('myPlant').src="resources/Plante2.png";
+    secondScene();
    }
 
    else if (thirstlevel <= 30) {
-    document.getElementById('output').innerHTML = "Moooore !"; document.getElementById('myPlant').src="resources/Plante1.png";
+    firstScene();
    }
   
 }
 
+
+
+// One function per scene //
+
+
+
+function firstScene() {
+  document.getElementById('intro').innerHTML = "Hi there!<br> I'm Eddie the plant, water me if you like :)"; 
+  document.getElementById('myPlant').src="resources/Plante1.png";
+  document.body.style.backgroundColor = "var(--lightblue)";
+  document.getElementById('credit').style.backgroundColor = "var(--green)";
+  document.documentElement.style.setProperty('--firstAndLastKeyFrames', "#deffff"); 
+  document.documentElement.style.setProperty('--secondKeyFrame', "#defee7"); 
+  thirstlevel = 20;
+  thirstGenerator();
+
+} 
+
+function secondScene() {
+  document.getElementById('intro').innerHTML = "Wow thanks!<br>I could drink more!"; 
+  document.getElementById('myPlant').src="resources/Plante2.png";
+  document.body.style.backgroundColor = "var(--lightgreen)";
+  document.getElementById('credit').style.backgroundColor = "var(--greenfooter)";
+  document.documentElement.style.setProperty('--firstAndLastKeyFrames', "#defee7"); 
+  document.documentElement.style.setProperty('--secondKeyFrame', "#f3fed6");
+} 
+
+function thirdScene() {
+  document.getElementById('intro').innerHTML = "Gloups...<br>"; 
+  document.getElementById('myPlant').src="resources/Plante3.png";
+  document.body.style.backgroundColor = "var(--lightyellow)";
+  document.getElementById('credit').style.backgroundColor = "var(--darkyellow)";
+  document.documentElement.style.setProperty('--firstAndLastKeyFrames', "#f3fed6"); 
+  document.documentElement.style.setProperty('--secondKeyFrame', "#e0f4ad");
+} 
+
+function fourthScene() {
+  document.getElementById('intro').innerHTML = "AaAaaAargh whyYyYyy human whyyyy?!<br>"; 
+  document.getElementById('myPlant').src="resources/Plante4.png";
+  document.body.style.backgroundColor = "var(--lightred)";
+  document.getElementById('credit').style.backgroundColor = "var(--darkred)";
+  document.documentElement.style.setProperty('--firstAndLastKeyFrames', "#e0f4ad"); 
+  document.documentElement.style.setProperty('--secondKeyFrame', "#fed5d5");
+} 
